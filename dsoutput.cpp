f1c99c38576c8df6765a74e6414a15fdae362f9d
Kevin Cox <kevincox.ca@gmail.com>
2012-01-16 18:53:19 -0500

    Moved constants out of the function.

--- dsoutput.cpp
+++ dsoutput.cpp
@@ -3,7 +3,7 @@
 DSOutput::DSOutput(IterativeRobot *robot, Drive *drive)
 {
 	this->drive = drive;
-	
+
 	init();
 }
 
@@ -12,24 +12,25 @@ void DSOutput::init()
 	out = DriverStationLCD::GetInstance();
 }
 
+const DriverStationLCD::Line l1 = DriverStationLCD::kUser_Line1;
+const DriverStationLCD::Line l2 = DriverStationLCD::kUser_Line2;
+const DriverStationLCD::Line l3 = DriverStationLCD::kUser_Line3;
+const DriverStationLCD::Line l4 = DriverStationLCD::kUser_Line4;
+const DriverStationLCD::Line l5 = DriverStationLCD::kUser_Line5;
+const DriverStationLCD::Line l6 = DriverStationLCD::kUser_Line6;
+
 void DSOutput::update()
 {
-	DriverStationLCD::Line l1 = DriverStationLCD::kUser_Line1;
-	DriverStationLCD::Line l2 = DriverStationLCD::kUser_Line2;
-	DriverStationLCD::Line l3 = DriverStationLCD::kUser_Line3;
-	DriverStationLCD::Line l4 = DriverStationLCD::kUser_Line4;
-	DriverStationLCD::Line l5 = DriverStationLCD::kUser_Line5;
-	DriverStationLCD::Line l6 = DriverStationLCD::kUser_Line6;
 
 /*
 	if      (robot->IsDisabled())        out->PrintfLine(l1, "Mode: Disabled");
 	else if (robot->IsAutonomous())      out->PrintfLine(l1, "Mode: Autonomous");
 	else if (robot->IsOperatorControl()) out->PrintfLine(l1, "Mode: Teleop");
 */
-	
+
 	out->PrintfLine(l2, "Drive %s reversed.", drive->isFlipped()?"IS":"NOT");
 	out->PrintfLine(l3, "L: % .4lf   R: % .4lf",
 	                 drive->getLeftSpeed(), drive->getRightSpeed());
-	
+
 	out->UpdateLCD();
 }
